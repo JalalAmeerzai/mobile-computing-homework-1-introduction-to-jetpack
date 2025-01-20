81d2d83c90a893814bb8aedff526b5ecc267482a
Jalal <jalalx@msn.com>
2025-01-20 21:20:59 +0200

    Used layouts to give a basic look and feel for a custom image and the texts

--- app/src/main/java/com/example/mobile_computing_homework_1_introduction_to_jetpack/MainActivity.kt
+++ app/src/main/java/com/example/mobile_computing_homework_1_introduction_to_jetpack/MainActivity.kt
@@ -1,29 +1,58 @@
 package com.example.mobile_computing_homework_1_introduction_to_jetpack
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("World")
+            MessageCard(Message("Jalal Ghaffar", "Hello Everyone!"))
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(vertical = 20.dp, horizontal = 10.dp)) {
+        Image(
+            painter = painterResource(R.drawable.user1),
+            contentDescription = "User1 profile picture",
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(10.dp))
+
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
 }
 
-@Preview
-@Composable
-fun PreviewMessageCard() {
-    MessageCard("World")
-}
+
+
+
+
+
+
