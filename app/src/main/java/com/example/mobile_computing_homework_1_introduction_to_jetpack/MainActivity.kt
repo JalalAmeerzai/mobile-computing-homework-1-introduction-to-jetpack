35ebc8435133e36555ec5b3d6591635dd2f0d820
Jalal <jalalx@msn.com>
2025-01-20 20:25:19 +0200

    Updated the boilerplate code

--- app/src/main/java/com/example/mobile_computing_homework_1_introduction_to_jetpack/MainActivity.kt
+++ app/src/main/java/com/example/mobile_computing_homework_1_introduction_to_jetpack/MainActivity.kt
@@ -3,45 +3,13 @@ package com.example.mobile_computing_homework_1_introduction_to_jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mobile_computing_homework_1_introduction_to_jetpack.ui.theme.Mobilecomputinghomework1introductiontojetpackTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Mobilecomputinghomework1introductiontojetpackTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Text("Hello world!")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Mobilecomputinghomework1introductiontojetpackTheme {
-        Greeting("World")
     }
 }
